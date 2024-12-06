f32a7d6b98ec96a4dbc1fe8099f178aab5c3979c
Deyao Ren <deyaoren@google.com>
2024-12-06 08:32:21 +0000

    Version bump to ZP1A.241121.001
    Change-Id: I148a87dc361dd7736640d2a8f13cde200df5a466

--- core/build_id.mk
+++ core/build_id.mk
@@ -18,8 +18,8 @@
 # (like "CRB01").  It must be a single word, and is
 # capitalized by convention.
 
-BUILD_ID=ZP1A.241118.001
+BUILD_ID=ZP1A.241121.001
 
 
 
-BUILD_ID:=${BUILD_ID}-ZP1A.241118.001
+BUILD_ID:=${BUILD_ID}-ZP1A.241121.001
